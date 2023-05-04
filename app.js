1f9ac005b9fa34d7c513590e0ba4cff29034adf4
Marcos Salinas <marcossalinas12nov@gmail.com>
2023-05-04 11:31:23 -0300

    calculadora

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 
 
 /// for (DESDE DONDE COMIENZA; CONDICION DE AVANCE; COMO AVANZA)
-for (let i= 1; i <= 10 ; i++) 
+/*for (let i= 1; i <= 10 ; i++) 
 {
     console.log(i);
 }
@@ -29,7 +29,7 @@ for (let i= 1; i <= 10 ; i++)
 
 promedio = suma / 10;
 
-console.log ('El promedio es:' + promedio)
+console.log ('El promedio es:' + promedio)*/
 
 
 const SUMA = '+';
@@ -41,24 +41,29 @@ let resultado;
 let continuar;
 
 do {
-   let operaacion = prompt('Ingrese la operacion que desea resalizar "+ - * /"')
+   let operacion = prompt('Ingrese la operacion que desea resalizar "+ - * /"');
    let numero1 = Number(prompt('ingrese primer numero'));
    let numero2 = Number(prompt('ingrese segundo numero'));
 
    switch(operacion) {
       case SUMA:
          resultado = numero1 + numero2;
+         break;
       case RESTA:
          resultado = numero1 - numero2;
+         break;
       case PRODUCTO:
          resultado = numero1 * numero2;
+         break;
       case DIVISION:
          resultado = numero1 / numero2;
+         break;
       default:
          resultado = NaN ///not a number
          console.log ('Ingrese un numero valido');
    }
 
+   console.log ('El resultado de la operacion es:' + resultado);
+   continuar = prompt('Desea continuar s/n');
 
-
-}while(continuar == "s")
+}while(continuar == "s");
